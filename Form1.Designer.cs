234a263b8a8cd0e13da593c5ccdb7120f628e66f
admin <admin@DESKTOP-GARPRVJ>
2023-05-10 19:26:15 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace LR_23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.buttonPlot = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введіть значення коефіцієнта а:";
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(238, 10);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(50, 22);
+            this.tbA.TabIndex = 1;
+            // 
+            // buttonPlot
+            // 
+            this.buttonPlot.Location = new System.Drawing.Point(350, 5);
+            this.buttonPlot.Name = "buttonPlot";
+            this.buttonPlot.Size = new System.Drawing.Size(113, 33);
+            this.buttonPlot.TabIndex = 2;
+            this.buttonPlot.Text = "Побудувати";
+            this.buttonPlot.UseVisualStyleBackColor = true;
+            this.buttonPlot.Click += new System.EventHandler(this.buttonPlot_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 44);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(858, 600);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(657, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 67);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 653);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonPlot);
+            this.Controls.Add(this.tbA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.Button buttonPlot;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR_23
+{
+    
+    public partial class Form1 : Form
+    {
+        private double a;
+        private void DrawGraph()
+        {
+            Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            // Очистити площу малювання
+            graphics.Clear(Color.White);
+
+            // Визначити область визначення
+            double tMin = -10;
+            double tMax = 10;
+
+            // Визначення розмірів площі малювання
+            int width = pictureBox.Width;
+            int height = pictureBox.Height;
+
+            // Масштабування
+            double scale = Math.Min(width / (tMax - tMin), height / (tMax - tMin));
+
+            // Малювання осей координат
+            Pen axisPen = new Pen(Color.Black, 2);
+            graphics.DrawLine(axisPen, 0, height / 2, width, height / 2); // Ось X
+            graphics.DrawLine(axisPen, width / 2, 0, width / 2, height); // Ось Y
+
+            // Малювання поділок на осях
+            Font font = new Font("Arial", 8);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            int divisionSize = 10;
+
+            for (int i = -width / 2; i < width / 2; i += divisionSize)
+            {
+                graphics.DrawLine(Pens.LightGray, width / 2 + i, 0, width / 2 + i, height);
+                graphics.DrawString((i / scale).ToString(), font, brush, width / 2 + i, height / 2 + 2);
+            }
+
+            for (int i = -height / 2; i < height / 2; i += divisionSize)
+            {
+                graphics.DrawLine(Pens.LightGray, 0, height / 2 + i, width, height / 2 + i);
+                graphics.DrawString((-i / scale).ToString(), font, brush, width / 2 + 2, height / 2 + i);
+            }
+
+            // Обчислити та намалювати точки на графіку
+            for (double t = tMin; t <= tMax; t += 0.01)
+            {
+                double x = a * (t - Math.Sin(t * t));
+                double y = a * (t - Math.Cos(t * t));
+
+                int pixelX = (int)(width / 2 + x * scale); // Масштабування і перетворення координат
+                int pixelY = (int)(height / 2 - y * scale); // Масштабування і перетворення координат
+
+                bitmap.SetPixel(pixelX, pixelY, Color.Black);
+            }
+
+            pictureBox.Image = bitmap;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonPlot_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = double.Parse(tbA.Text); // Отримати значення коефіцієнта a
+                DrawGraph();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка: " + ex.Message);
+            }
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
